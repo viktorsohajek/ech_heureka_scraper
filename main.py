4604817267dd2b60c616c4df95db2c4bf4dae3df
viktorsohajek <sohajek.viktor@gmail.com>
2015-11-13 18:21:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 import sys
 import csv
 
-inFilePath = '/data/in/tables/source.csv'
-outFilePath = '/data/out/tables/destination.csv'
-csvDelimiter = ','
-csvQuoteChar = '"'
-
-with open(inFilePath, "rt") as inFile, open(outFilePath, 'wt') as outFile:
-	reader = csv.reader(inFile, delimiter = csvDelimiter, quotechar = csvQuoteChar)
-	writer = csv.writer(outFile, delimiter = csvDelimiter, quotechar = csvQuoteChar, quoting = csv.QUOTE_MINIMAL)
-	for row in reader:
-		writer.writerow(['batman', row[0], row[1]])
+print "Hello world test"
