194aab1cf9e9454e0858e8829e432d2303623102
viktorsohajek <sohajek.viktor@gmail.com>
2015-12-06 00:17:01 +0100

    Debug cut

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import mechanize
 import cookielib
 from BeautifulSoup import BeautifulSoup
@@ -51,48 +49,3 @@ soup1 = BeautifulSoup(page)
 tabulka = soup1('table',{'class':'shop-list roi'})
 
 rows = BeautifulSoup(str(tabulka)).findChildren(['tr'])
-
-L = [] # deklarujeme prazdny list s vysledky
-for row in rows:
-    cells = row.findChildren('td')
-    cells = cells[0:4] #chceme jen jmeno vyhledavace a prvni tri hodnty
-    if len(cells) >= 4 :
-        # costs cisteni a uprava
-        costsWithCurrency = cells[3].string.split('&')
-        costs = float(costsWithCurrency[0].replace(',','.'))
-        currency = costsWithCurrency[1]
-        if currency == u'nbsp;Kč' :
-            currency = 'CZK'
-            #doplnit eura
-        # cpc cisteni a uprava
-        cpcWithCurrency = cells[2].string.split('&')
-        cpc = float(cpcWithCurrency[0].replace(',','.'))
-        # visits cisteni a uprava
-        visits = float(cells[1].string)
-        # name cisteni a uprava
-        name = cells[0].string
-        if name == None :
-            name = 'Heureka.cz'
-
-        prvekL = {'shop':shop,
-                  'date':date,
-                  'name':name,
-                  'visits':visits,
-                  'cpc':cpc,
-                  'costs':costs,
-                  'currency':currency}
-        L.append(prvekL)
-    #for cell in cells:
-    #    value = cell.string
-    #    prvekL.append(value)
-
-#print L
-
-keys = ['name', 'visits', 'cpc', 'costs', 'currency','shop','date']
-#csv.register_dialect('singlequote', quotechar="'", quoting=csv.QUOTE_ALL)
-#csv.register_dialect('escaped', escapechar='\\', doublequote=False, quoting=csv.QUOTE_NONE)
-
-with open('result.csv', 'wb') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys, quoting=csv.QUOTE_NONNUMERIC)
-    dict_writer.writeheader()
-    dict_writer.writerows(L)
