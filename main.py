ffdfed199c30c5b05d43753af9d61de70637e49b
viktorsohajek <sohajek.viktor@gmail.com>
2015-12-07 10:59:18 +0100

    list of shops and dates

--- main.py
+++ main.py
@@ -5,6 +5,45 @@ import cookielib
 from BeautifulSoup import BeautifulSoup
 import html2text
 import csv # pro exportovani do CSV
+
+# date input
+from datetime import date, timedelta
+yesterday = date.today() - timedelta(1)
+date = yesterday.strftime('%Y-%m-%d')
+
+#list shopu
+dict={}
+dict['Heureka.cz'] = {}
+dict['Zbozi.cz'] = {}
+dict['Heureka.cz']['Login_1'] = {'Login': 'valiska@sportmall.cz',
+                     'Password': 'heurech15',
+                     'Url_login': 'https://login.heureka.cz/login',
+                     'Shop': ['Spory.cz','Snowboards.cz','Kolonial.cz','Prodeti.cz'],
+                     'Url_stats': ['http://sluzby.heureka.cz/obchody/statistiky/?shop=5709&from='+date+'&to='+date+'&cat=-4','http://sluzby.heureka.cz/obchody/statistiky/?shop=1786&from='+date+'&to='+date+'&cat=-4','http://sluzby.heureka.cz/obchody/statistiky/?shop=53090&from='+date+'&to='+date+'&cat=-4','http://sluzby.heureka.cz/obchody/statistiky/?shop=45555&from='+date+'&to='+date+'&cat=-4'] }
+
+dict['Heureka.cz']['Login_2'] = {'Login': 'heureka@bigbrands.cz',
+                     'Password': 'ecommerceheureka',
+                     'Url_login': 'https://login.heureka.cz/login',
+                     'Shop': ['BigBrands.cz'],
+                     'Url_stats': ['http://sluzby.heureka.cz/obchody/statistiky/?from='+date+'&to='+date+'&shop=42893&cat=-4'] }
+
+dict['Heureka.cz']['Login_3'] = {'Login': 'info@limal.cz',
+                     'Password': 'heureka123456',
+                     'Url_login': 'https://login.heureka.cz/login',
+                     'Shop': ['Rozbaleno.cz'],
+                     'Url_stats': ['http://sluzby.heureka.cz/obchody/statistiky/?from='+date+'&to='+date+'&shop=6590&cat=-4'] }
+
+dict['Zbozi.cz']['Login_1'] = {'Login': 'it@snowboards.cz',
+                     'Password': 'wsappcsk25',
+                     'Url_login': '',
+                     'Shop': ['Sporty.cz','Snowboards.cz','Prodeti.cz'],
+                     'Shop_id': ['15801','9072','109963'],
+                     'Url_stats': ['http://sluzby.heureka.cz/obchody/statistiky/?from='+date+'&to='+date+'&shop=6590&cat=-4'] }
+
+
+
+
+
 # Browser
 br = mechanize.Browser()
 
@@ -86,13 +125,12 @@ for row in rows:
     #    value = cell.string
     #    prvekL.append(value)
 
-#print L
 
 keys = ['name', 'visits', 'cpc', 'costs', 'currency','shop','date']
 #csv.register_dialect('singlequote', quotechar="'", quoting=csv.QUOTE_ALL)
 #csv.register_dialect('escaped', escapechar='\\', doublequote=False, quoting=csv.QUOTE_NONE)
 
-with open('/data/out/tables/result.csv', 'wb') as output_file:
+with open('/data/out/tables/heureka_kn.csv', 'wb') as output_file:
     dict_writer = csv.DictWriter(output_file, keys, quoting=csv.QUOTE_NONNUMERIC)
     dict_writer.writeheader()
     dict_writer.writerows(L)
